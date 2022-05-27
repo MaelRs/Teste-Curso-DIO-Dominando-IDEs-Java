5330260cd54fa24cfb6ef9e4384b3332b24f23b9
MaelRs <robertdossantos.ismael@gmail.com>
2022-05-27 09:28:52 -0300

    first commit intelij

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+      Gato gato=new Gato();
+        System.out.println(gato);
+        /*  int a=6;
+        int b=3;
+        int s=a+b;
+        System.out.println("A soma entre "+a+" e "+b+" é igual a :"+s);
+        System.out.println("Hello World!");*/
+    }
+}
